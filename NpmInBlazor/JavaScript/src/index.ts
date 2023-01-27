7fac63646e200c6716ed455988778729cdffa561
Erik Seibel <erik.seibel@adesso.de>
2023-01-27 09:16:55 +0100

    add example npm package and usage (chart.js)

--- NpmInBlazor/JavaScript/src/index.ts
+++ NpmInBlazor/JavaScript/src/index.ts
@@ -1,7 +1,26 @@
-﻿interface test {
-    a: string,
-    b: number
-}
+﻿import Chart from 'chart.js/auto';
 
-const tester: test = {a: 'asdads', b: 92983};
-console.log(tester);
+// noinspection JSUnusedGlobalSymbols
+export function showChart(data: number[]) {
+    const ctx = document.getElementById('chart');
+
+    // @ts-ignore
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: data,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
